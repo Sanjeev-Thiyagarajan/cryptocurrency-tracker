0fbbedcf7e6805066313f59b01772dc91402be6b
Sanjeev Thiyagarajan <sanjeevkt720@gmail.com>
2020-07-16 10:59:25 -0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-center text-warning mt-3 mb-4">Coinstar</h1>
+    </div>
+  );
+};
+
+export default Header;
