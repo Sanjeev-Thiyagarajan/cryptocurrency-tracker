3b50c711744f795563980d7b1c10957b541dd3ca
Andrew Blake <82421095+andblake22@users.noreply.github.com>
2022-02-22 07:14:08 -0600

    fix error with toLocaleString method and null value from API response
    added ternary operator in CoinData rendering to show N/A when API returns null for a Coin's data

--- src/components/CoinData.jsx
+++ src/components/CoinData.jsx
@@ -3,32 +3,31 @@ import React from "react";
 const CoinData = ({ data }) => {
   const renderData = () => {
     if (data) {
-      console.log(data)
       return (
         <div className="bg-white mt-3 p-2 rounded border row">
           <div className="col-sm">
             <div className="d-flex flex-column">
               <span className="text-muted coin-data-category">Market Cap</span>
-              <span>{data.market_cap.toLocaleString()}</span>
+              <span>{data.market_cap ? data.market_cap.toLocaleString() : 'N/A'}</span>
             </div>
             <hr />
             <div className="d-flex flex-column">
               <span className="text-muted coin-data-category">
                 Total Supply
               </span>
-              <span>{data.total_supply.toLocaleString()}</span>
+              <span>{data.total_supply ? data.total_supply.toLocaleString() : 'N/A'}</span>
             </div>
           </div>
 
           <div className="col-sm">
             <div className="d-flex flex-column">
               <span className="text-muted coin-data-category">Volume(24H)</span>
-              <span>{(data.total_volume).toLocaleString()}</span>
+              <span>{(data.total_volume ? data.total_volume.toLocaleString() : 'N/A')}</span>
             </div>
             <hr />
             <div className="d-flex flex-column">
               <span className="text-muted coin-data-category">high 24h</span>
-              <span>{data.high_24h.toLocaleString()}</span>
+              <span>{data.high_24h ? data.high_24h.toLocaleString() : 'N/A'}</span>
             </div>
           </div>
 
@@ -37,12 +36,12 @@ const CoinData = ({ data }) => {
               <span className="text-muted coin-data-category">
                 Circulating Supply
               </span>
-              <span>{data.circulating_supply.toLocaleString()}</span>
+              <span>{data.circulating_supply ? data.circulating_supply.toLocaleString() : 'N/A'}</span>
             </div>
             <hr />
             <div className="d-flex flex-column">
               <span className="text-muted coin-data-category">low 24h</span>
-              <span>{data.low_24h.toLocaleString()}</span>
+              <span>{data.low_24h ? data.low_24h.toLocaleString() : 'N/A'}</span>
             </div>
           </div>
         </div>
